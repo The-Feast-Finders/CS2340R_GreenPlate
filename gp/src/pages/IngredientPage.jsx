fb18ecaea4009ee86dbbac87e8355b32c85752b1
lucasarano <larano3@gatech.edu>
2024-03-26 14:32:18 +0100

    created simple add ingredient page

--- gp/src/pages/IngredientPage.jsx
+++ gp/src/pages/IngredientPage.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavigationBar from '../components/NavigationBar';
+import InputIngredient from '../components/InputIngredient';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
 
 
 const IngredientPage = () => {
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const auth = getAuth();
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        });
+        // Took out unsubscribe
+        return () => unsubscribe();
+    }, []);
     return (
         <div>
             <NavigationBar />
             <div className='frame'> 
                 <h1>Ingredient Page</h1>
                 <p>Welcome to the Ingredient Page!</p>
-            </div>        
+            </div>
+            <InputIngredient user={user}/>        
         </div>
     );
 };
